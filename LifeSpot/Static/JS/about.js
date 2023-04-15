578fb9855c7d1cd475704f39aaf6128dd63cbc6d
alexhrinyov <hrinyou29@gmail.com>
2023-04-15 19:15:07 +0300

    15/04

--- LifeSpot/Static/JS/about.js
+++ LifeSpot/Static/JS/about.js
@@ -1,29 +1,97 @@
-﻿/*
-* Оставить комментарий
+﻿
+/*
+ 
+/*
+* Запишем объект на страницу
 *
 * */
+function ButonInfo(Id) {
+    id = Id;
+};
 
-let reviewCounter = -1;
-function getComment() {
-    // Создаем объект обычного комментария
-    let comment = {}
+const writeReview = review => {
+    let likeCounter = '';
+    
+    let ButtonId = Math.random();
+    
+    // Для проверки, является ли объект отзывом, используем свойство hasOwnProperty
+    if (review.hasOwnProperty('rate')) {
+        likeCounter += '<button id="' + ButtonId + '" style="border: none" onclick="addLike(this.id)">' + `❤️ ${review.rate}</button>`
+    }
+    
+    // Запишем результат
+    
+    document.getElementsByClassName('reviews')[0].innerHTML += '    <div class="review-text">\n' +
+        `<p> <i> <b>${review['author']}</b>  ${review['date']}${likeCounter}    </i></p>` +
+        `<p>${review['text']}</p>` +
+        '</div>';
+   
+}
 
+function addLike(ButtonId) {
+    
+    // Найдём нужный элемент по id
+    let element = document.getElementById(ButtonId);
+
+    // Преобразуем текст элемента в массив, разбив его по пробелам (так как счётчик лайков у нас отделен от символа ❤️пробелом)
+    let array = element.innerText.split(' ')
+
+    // Вытащим искомое значение счётчика и сразу же преобразуем его в число, так как
+    // при сложении любого значения со строкой в JS будет строка, а нам этого не требуется
+    let resultNum = parseInt(array[array.length - 1], 10);
+
+    // Увеличим счётчик
+    resultNum += 1
+
+    // Сохраним измененное значение обратно в массив
+    array[array.length - 1] = `${resultNum}`
+
+    // Обновим текст элемента
+    element.innerText = array.join(' ') 
+}
+
+
+
+
+/*
+* Конструктор, через который создаётся комментарий
+*
+* */
+function Comment() {
     // Запросим имя
-    comment.author = prompt("Как вас зовут ?")
-    if (comment.author == null) {
+    this.author = prompt("Как вас зовут ?")
+    if (this.author == null) {
+        this.empty = true
         return
     }
 
     // Запросим текст
-    comment.text = prompt("Оставьте отзыв")
-    if (comment.text == null) {
+    this.text = prompt("Оставьте отзыв")
+    if (this.text == null) {
+        this.empty = true
         return
     }
 
     // Сохраним текущее время
-    comment.date = new Date().toLocaleString()
+    this.date = new Date().toLocaleString()
+}
 
-    // Запросим,, хочет ли пользователь оставить полноценный отзыв или это будет обычный комментарий
+
+/*
+* Оставить комментарий
+*
+* */
+
+
+function addComment() {
+    let comment = new Comment()
+    
+    // проверяем, успешно ли юзер осуществил ввод
+    if (comment.empty) {
+        return;
+    }
+
+    // Запросим, хочет ли пользователь оставить полноценный отзыв или это будет обычный комментарий
     let enableLikes = confirm('Разрешить пользователям оценивать ваш отзыв?')
 
     if (enableLikes) {
@@ -32,39 +100,12 @@ function getComment() {
         // и добавим ему нужное свойство
         review.rate = 0;
 
+        
+
         // Добавляем отзыв с возможностью пользовательских оценок
         writeReview(review)
     } else {
         // Добавим простой комментарий без возможности оценки
         writeReview(comment)
     }
-}
-
-/*
- 
-/*
-* Запишем объект на страницу
-*
-* */
-const writeReview = review => {
-    let likeCounter = '';
-
-    // Для проверки, является ли объект отзывом, используем свойство hasOwnProperty
-    if (review.hasOwnProperty('rate')) {
-        likeCounter += '<b style="color: chocolate">Рейтинг:</b> ' + `<b class = "Rate"> ${review.rate}</b>` ;
-    }
-
-    // Запишем результат
-    document.getElementsByClassName('reviews')[0].innerHTML += '    <div class="review-text">\n' +
-        `<p> <i> <b>${review['author']}</b>  ${review['date']}${likeCounter}   <button onclick = increaseRate()>+</button> </i></p>` +
-        `<p>${review['text']}</p>` +
-        '</div>';
-    reviewCounter++;
-}
-
-function increaseRate()  {
-    debugger
-    document.getElementsByClassName('Rate')[reviewCounter].innerHTML = `<b> ${Number(document.getElementsByClassName('Rate')[reviewCounter].textContent) + 1}</b>`;
-    
-    
 }
